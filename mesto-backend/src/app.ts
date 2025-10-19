4164523c535c637647a8a33a9cd6524954185533
Алекс Рассудихин <76869388+punkmachine@users.noreply.github.com>
2025-10-19 11:07:58 +0500

    feat: добавил тудушки про логгеры и обработчики ошибок на будущее

--- mesto-backend/src/app.ts
+++ mesto-backend/src/app.ts
@@ -2,6 +2,7 @@ import 'dotenv/config';
 
 import express, { Request, Response } from 'express';
 import helmet from 'helmet';
+// import celebrate from 'celebrate';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -12,6 +13,13 @@ app.use(helmet());
 app.get('/', (req: Request, res: Response) => {
   res.json({ message: 'Сервер доступен!' });
 });
+
+// тут надо подключить логгер ошибок из мидлварок на winston
+
+// обработчик ошибок валидации запросов
+// app.use(celebrate.errors());
+
+// тут надо подключить кастомный обработчик ошибок 500
 
 app.listen(PORT, () => {
   console.log(`Сервер запущен на порту ${PORT}`);
