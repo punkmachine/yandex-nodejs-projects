febe438154d764317e5c2fb0e13ad8533ccfd07c
Алекс Рассудихин <76869388+punkmachine@users.noreply.github.com>
2025-10-19 11:48:17 +0500

    feat: валидация запросов на celebrate

---
+++ mesto-backend/src/helpers/userValidations.ts
@@ -0,0 +1,15 @@
+import { celebrate, Joi } from 'celebrate';
+
+export const createUserValidation = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    about: Joi.string().required().min(2).max(200),
+    avatar: Joi.string().required().uri().pattern(/^https?:\/\//),
+  }),
+});
+
+export const getUserByIdValidation = celebrate({
+  params: Joi.object().keys({
+    id: Joi.string().required().length(24).hex(),
+  }),
+});
